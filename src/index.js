a9a019850a403cd63d6b93c7906cee45098d2b76
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-10-31 01:07:23 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,37 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Directory from './components/directory';
 import { Footer } from 'lilw-react-components'; // Import the footer component
+import './your-css-file.css'; // Import your custom CSS file
 
 ReactDOM.render([
   <heading>
-  <div className="custom-header-container">
-    <div className="custom-logo">
-      <a href="https://swansea.ac.uk">
-        <img
-          src="https://intranet.swan.ac.uk/cdn/suds/images/logo/application-custom/logo-white-en.png"
-          className="swansea-logo"
-          alt="Swansea University Logo"
-        />
-      </a>
+    <div className="custom-header-container" style={{ width: '100%' }}>
+      <div className="custom-logo">
+        <a href="https://swansea.ac.uk">
+          <img
+            src="https://intranet.swan.ac.uk/cdn/suds/images/logo/application-custom/logo-white-en.png"
+            className="swansea-logo"
+            alt="Swansea University Logo"
+          />
+        </a>
+      </div>
+      <div className="customer-header-text">
+        <h2 className="custom-header-text">Directory of Expertise</h2>
+      </div>
+      <div className="custom-logo">
+        <a href="https://legaltech.wales">
+          <img
+            src="/static/media/lilw-logo-white.2c58d3c3.png"
+            className="lilw-logo"
+            alt="LILW Logo"
+          />
+        </a>
+      </div>
     </div>
-    <div className="customer-header-text">
-            <h2 className="custom-header-text">Directory of Expertise</h2>
+  </heading>,
+  <main>
+    <div className="table-responsive">
+      <Directory />
     </div>
-    <div className="custom-logo">
-      <a href="https://legaltech.wales">
-        <img
-          src="/static/media/lilw-logo-white.2c58d3c3.png"
-          className="lilw-logo"
-          alt="LILW Logo"
-        />
-      </a>
-    </div>
-    
-  </div></heading>,
-    <main>
-    <Directory/>
   </main>,
-  <Footer/>
+  <Footer />,
   ],
   document.getElementById('root')
 );
