bb582795fbf1fa80b1291716062729d72e9ac685
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-02 23:01:13 +0000

    Update directory.js
    change join layout for expertise

--- src/components/directory.js
+++ src/components/directory.js
@@ -1,96 +1,78 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import './directory.scss';
-import Filter from './filter';
-
-function formatTitle(title) {
-  const titleParts = title.split(',')[0].trim().toLowerCase().split(' ');
-  return titleParts.map((part) => part.charAt(0).toUpperCase() + part.slice(1)).join(' ');
-}
+import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
+import './directory.scss'
+import Filter from "./filter"
 
 function DirectoryRow(props) {
-  const formattedTitle = formatTitle(props.staff_member.title);
-
   return (
-    <>
-      {props.staff_member.expertise.length > 0 && (
-        <tr className={`${props.college.key} ${props.department.key}`}>
-          <td nowrap='true' className='staff-name'>
-            <a href={props.staff_member.url} target='_blank' rel='noreferrer'>
-              {props.staff_member.name}
-            </a>
-            <br />
-            {formattedTitle} {/* Display the formatted title */}
-          </td>
-          <td className='college'>{props.college.name}</td>
-          <td>{props.department.name}</td>
-          <td>{props.staff_member.expertise.join(', ')}</td>
-        </tr>
-      )}
-    </>
-  );
+    <tr className={`${props.college.key} ${props.department.key}`}>
+      <td nowrap='true' className='staff-name'>
+        <a href={props.staff_member.url} target='_blank' rel='noreferrer'>{props.staff_member.name}</a></td>
+      <td className='college'>{props.college.name}</td>
+      <td>{props.department.name}</td>
+      <td>
+        <ul>
+          {props.staff_member.expertise.map((expertise, index) => (
+            <li key={index}>{expertise}</li>
+          ))}
+        </ul>
+      </td>
+    </tr>
+  )
 }
 
 export default class Directory extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       last_update: '',
-      colleges: [],
-    };
+      colleges: []
+    }
   }
 
   componentDidMount() {
     fetch('/expertise.json')
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ ...this.state, ...data });
-      })
-      .catch((err) => console.error(err));
+        .then(response => response.json())
+        .then(data => {
+          this.setState({...this.state, ...data})
+        })
+        .catch(err => console.error(err))
   }
 
   render() {
     return (
-      <Container>
-        <div className='title-div text-center'>
-            <h2>Directory of Expertise</h2>
-        <div className='update-div text-center'>
-            Last Updated {this.state.last_update}
-        </div>
-      </div>
-        <Row>
-          <Col xs={12}>
-            <Filter data={this.state} />
-            <table className='table'>
-              <thead>
+        <Container>
+          <Row>
+            <Col xs={12}>
+              <h1 className='title text-center'><strong>Directory of Expertise</strong></h1>
+              <h5 className='text-center'>This is a quick proof of concept that automatically collates the Areas of
+                Expertise from staff's profile pages on the main University website.</h5>
+              <Filter data={this.state}/>
+              <div>Last Updated at: {this.state.last_update}</div>
+              <table className='table'>
+                <thead>
                 <tr>
-                  <th scope='col'>Name / Employee Title</th>
-                  <th scope='col'>School</th>
+                  <th scope='col'>Name</th>
+                  <th scope='col'>College</th>
                   <th scope='col'>Department</th>
-                  <th scope='col'>Areas of Expertise</th>
+                  <th scope='col'>Expertise</th>
                 </tr>
-              </thead>
-              <tbody id='table-body'>
-                {this.state.colleges.map((college) => {
-                  return college['departments'].map((department) => {
-                    return department['staff'].map((staff_member) => {
-                      return (
-                        <DirectoryRow
-                          key={`${college.key}_${department.key}_${staff_member.name.toLowerCase().replaceAll(' ', '_')}`}
-                          college={college}
-                          department={department}
-                          staff_member={staff_member}
-                        />
-                      );
-                    });
-                  });
-                })}
-              </tbody>
-            </table>
-          </Col>
-        </Row>
-      </Container>
-    );
+                </thead>
+                <tbody id='table-body'>
+                  { this.state.colleges.map(college => {
+                    return college['departments'].map(department => {
+                      return department['staff'].map(staff_member => {
+                        return <DirectoryRow key={`${college.key}_${department.key}_${staff_member.name.toLowerCase().replaceAll(' ', '_')}`}
+                                             college={college} department={department} staff_member={staff_member}/>
+                      })
+                    })
+                  }) }
+                </tbody>
+              </table>
+            </Col>
+          </Row>
+        </Container>
+    )
   }
 }
