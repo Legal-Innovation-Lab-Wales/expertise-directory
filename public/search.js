a55af79766742be03af7bd517bf07627f8753518
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 01:40:48 +0000

    Update search.js

--- public/search.js
+++ public/search.js
@@ -4,16 +4,18 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
   $scope.results = [];
   $scope.filteredResults = [];
 
-  function searchPage(searchTerm, start = 1) {
-    const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
-    return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
-      .then(response => {
-        $scope.results = $scope.results.concat(response.data);
-        $scope.filteredResults = $scope.results;
-        $scope.$apply();  // Ensure UI updates
-        return response.data.length > 0 ? searchPage(searchTerm, start + 10) : $scope.results;
-      });
-  }
+    function searchPage(searchTerm, start = 1) {
+        const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
+        $scope.loading = true; // Set loading to true when fetching data
+        return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
+            .then(response => {
+                $scope.results = $scope.results.concat(response.data);
+                $scope.filteredResults = $scope.results;
+                $scope.loading = false; // Set loading to false when data has been fetched
+                $scope.$apply();
+                return response.data.length > 0 ? searchPage(searchTerm, start + 10) : $scope.results;
+            });
+    }
 
   $scope.search = function () {
     $scope.results = [];
