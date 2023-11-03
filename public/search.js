4321c7886f24574614a215ba11c0f061ccfc0aee
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 12:50:07 +0000

    Update search.js

--- public/search.js
+++ public/search.js
@@ -3,21 +3,24 @@ const app = angular.module('SearchApp', []);
 app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
   $scope.results = [];
   $scope.filteredResults = [];
-  $scope.totalResults = 0;  // Initialize totalResults
-  
+  $scope.totalResults = 0;
+  $scope.errorMessage = '';  
+
   $scope.search = function() {
     $scope.loading = true;
     $scope.results = [];
-    $scope.totalResults = 0; 
+    $scope.totalResults = 0;
+    $scope.errorMessage = '';  // Reset error message
     const searchTerm = $scope.searchTerm;
 
     searchPage(searchTerm).then(function(results) {
       $scope.results = results;
-      $scope.totalResults = $scope.results.length; // Ensure totalResults is updated
+      $scope.totalResults = $scope.results.length;
       $scope.filterResults();
 
     }).catch(function(error) {
       console.error("Error fetching data", error);
+      $scope.errorMessage = 'Failed to fetch data. Please try again.';  // Set error message
 
     }).finally(function() {
       $scope.loading = false;
@@ -42,7 +45,11 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
 
   function searchPage(searchTerm, start = 1) {
     const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
-    return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
+    const LOCAL_ENDPOINT = `http://localhost:5000/mockApi?q=${encodeURIComponent(searchTerm)}&s=${(start - 1) * 10}`;
+    const isLocal = window.location.hostname === 'localhost';
+    const url = isLocal ? LOCAL_ENDPOINT : FUNCTION_ENDPOINT;
+    
+    return $http.get(url, { params: { q: searchTerm, s: start } })
       .then(response => {
         if (!response.data || response.data.length === 0) {
           return $scope.results;
