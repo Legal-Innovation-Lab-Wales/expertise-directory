3a34dbcf8eeb8c1b9b70fd779d9759c3664709a3
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 03:05:19 +0000

    Update search.js

--- public/search.js
+++ public/search.js
@@ -4,24 +4,30 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
   $scope.results = [];
   $scope.filteredResults = [];
   $scope.totalResults = 0;
+  $scope.maxPages = 0;  // New variable to track the maximum number of pages
 
   function searchPage(searchTerm, start = 1) {
     const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
     $scope.loading = true;
     return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
       .then(response => {
-        const { results, totalPages } = response.data;
-        $scope.results = $scope.results.concat(results);
+        // Assuming response.data contains results, totalResults, and maxPages
+        $scope.results = $scope.results.concat(response.data.results);
+        $scope.totalResults = response.data.totalResults;
+        $scope.maxPages = response.data.maxPages;  // Update max pages
         $scope.filteredResults = $scope.results;
-        $scope.totalResults = totalPages * 10; // Assuming there are 10 results per page
         $scope.loading = false;
         $scope.$apply();
-        return results.length > 0 && start < $scope.totalResults ? searchPage(searchTerm, start + 10) : $scope.results;
+
+        // Adjust the condition to stop fetching after reaching max pages
+        return start / 10 < $scope.maxPages ? searchPage(searchTerm, start + 10) : $scope.results;
       });
   }
 
   $scope.search = function () {
     $scope.results = [];
+    $scope.totalResults = 0;
+    $scope.maxPages = 0;  // Reset max pages on new search
     const searchTerm = $scope.searchTerm;
     searchPage(searchTerm);
   };
@@ -33,5 +39,6 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
       result.additionalInfo.toLowerCase().includes(additionalSearchTerm) ||
       (result.expertise && result.expertise.some(e => e.toLowerCase().includes(additionalSearchTerm)))
     );
+    $scope.totalResults = $scope.filteredResults.length;
   };
 }]);
