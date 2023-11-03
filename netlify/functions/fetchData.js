e597b9693f5e26cb5f1b534cfd891e65ed7f2f79
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 13:26:17 +0000

    Update fetchData.js

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -3,9 +3,8 @@ const cheerio = require('cheerio');
 
 console.log('entering fetch');
 
-// Export the fetchPageResults function
-exports.fetchPageResults = async function (url) {
-  console.log(`Fetching data from URL: ${url}`); // Log the URL being fetched
+async function fetchPageResults(url) {
+  console.log(`Fetching data from URL: ${url}`);
   const { data } = await axios.get(url);
   const $ = cheerio.load(data);
 
@@ -19,7 +18,7 @@ exports.fetchPageResults = async function (url) {
     let photoAlt = '';
 
     try {
-      console.log(`Fetching data for profile URL: ${profileUrl}`); // Log the profile URL being fetched
+      console.log(`Fetching data for profile URL: ${profileUrl}`);
       const { data: profileData } = await axios.get(profileUrl);
       const profile$ = cheerio.load(profileData);
 
@@ -30,20 +29,19 @@ exports.fetchPageResults = async function (url) {
       photoUrl = profile$('.staff-profile-overview-profile-picture img').attr('src');
       photoAlt = profile$('.staff-profile-overview-profile-picture img').attr('alt');
 
-      // Prepend the domain if the URL is a relative path
       if (photoUrl && !photoUrl.startsWith('http')) {
         photoUrl = `https://www.swansea.ac.uk${photoUrl}`;
       }
 
     } catch (error) {
-      console.error(`Failed to fetch details for ${profileUrl}`, error); // Log any errors
+      console.error(`Failed to fetch details for ${profileUrl}`, error);
     }
 
     return { name, profileUrl, additionalInfo, expertise, photoUrl, photoAlt };
   }).get());
 
   return results;
-};
+}
 
 exports.handler = async function (event) {
   const searchTerm = event.queryStringParameters.q;
@@ -51,45 +49,34 @@ exports.handler = async function (event) {
   console.log('trying to find pagination');
 
   try {
-    // Fetch the first page to get the total number of pages
     const firstPageUrl = `${baseUrl}&s=0`;
-    console.log(`Fetching data from first page URL: ${firstPageUrl}`); // Log the first page URL being fetched
+    console.log(`Fetching data from first page URL: ${firstPageUrl}`);
     const { data: firstPageData } = await axios.get(firstPageUrl);
     const $ = cheerio.load(firstPageData);
 
-    // Extract total pages information from pagination
     let totalPages = 1;
     $('ul.site-search-results-pagination li.site-search-results-pagination-item').each((i, el) => {
       const pageNum = parseInt($(el).find('a.site-search-results-pagination-item-link').text(), 10);
       totalPages = Math.max(totalPages, pageNum || 1);
     });
 
-    console.log('Total pages:', totalPages); // Debug statement
+    console.log('Total pages:', totalPages);
 
-      // Inside the handler function
-      const allPagesPromises = Array.from({ length: totalPages }, (_, i) => {
-        const s = 1 + 10 * (Math.pow(2, i) - 1); // Calculate s as per your requirement
-        
-        // Construct a URL object to manipulate query parameters
-        const url = new URL(baseUrl);
-        
-        // Set or overwrite the 's' query parameter
-        url.searchParams.set('s', s);
-        
-        // Fetch results using the modified URL
-        return fetchPageResults(url.toString());
-      });
+    const allPagesPromises = Array.from({ length: totalPages }, (_, i) => {
+      const pageUrl = `${baseUrl}&s=${i * 10}`;
+      return fetchPageResults(pageUrl);
+    });
 
     const allResults = (await Promise.all(allPagesPromises)).flat();
 
-    console.log('Total records:', allResults.length); // Debug statement
+    console.log('Total records:', allResults.length);
 
     return {
       statusCode: 200,
       body: JSON.stringify(allResults),
     };
   } catch (error) {
-    console.error('Error fetching page results:', error); // Log any errors
+    console.error('Error fetching page results:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed fetching data' }),
