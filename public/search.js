c3c8ed766ce074bb0512c351503baeffc07f0eda
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 00:47:05 +0000

    Update search.js

--- public/search.js
+++ public/search.js
@@ -1,15 +1,36 @@
-const app = angular.module('SearchApp', []);
+angular.module('SearchApp', [])
+    .controller('SearchController', ['$scope', '$http', function($scope, $http) {
+        $scope.searchTerm = "";
+        $scope.results = [];
+        $scope.filteredResults = [];
+        $scope.page = 1;
+        $scope.additionalSearchTerm = "";
 
-app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
-  $scope.results = [];
+        $scope.search = function() {
+            // Recursive function to fetch data from each page
+            function fetchPage(page) {
+                var url = `/expertise.json?page=${page}&query=${$scope.searchTerm}`;
+                $http.get(url).then(function(response) {
+                    $scope.results = $scope.results.concat(response.data.results);
+                    if (response.data.nextPage) {
+                        fetchPage(page + 1);
+                    } else {
+                        $scope.filterResults();  // Filtering once all data is fetched
+                    }
+                });
+            }
 
-  $scope.search = function () {
-    const searchTerm = $scope.query;
-    const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
-    $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm } })
-      .then(response => {
-        $scope.results = response.data;
-      })
-      .catch(error => console.error('Error:', error));
-  };
-}]);
+            fetchPage(1);  // Start fetching from page 1
+        };
+
+        $scope.filterResults = function() {
+            if (!$scope.additionalSearchTerm) {
+                $scope.filteredResults = $scope.results;
+                return;
+            }
+
+            $scope.filteredResults = $scope.results.filter(function(item) {
+                return item.name.toLowerCase().includes($scope.additionalSearchTerm.toLowerCase());
+            });
+        };
+    }]);
