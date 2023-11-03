827c2db7e032e1e287cb5119540ad565105390eb
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 13:06:54 +0000

    Update search.js

--- public/search.js
+++ public/search.js
@@ -4,13 +4,13 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
   $scope.results = [];
   $scope.filteredResults = [];
   $scope.totalResults = 0;
-  $scope.errorMessage = '';  
+  $scope.errorMessage = '';
 
   $scope.search = function() {
     $scope.loading = true;
     $scope.results = [];
     $scope.totalResults = 0;
-    $scope.errorMessage = '';  // Reset error message
+    $scope.errorMessage = '';
     const searchTerm = $scope.searchTerm;
 
     searchPage(searchTerm).then(function(results) {
@@ -20,7 +20,7 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
 
     }).catch(function(error) {
       console.error("Error fetching data", error);
-      $scope.errorMessage = 'Failed to fetch data. Please try again.';  // Set error message
+      $scope.errorMessage = 'Failed to fetch data. Please try again.';
 
     }).finally(function() {
       $scope.loading = false;
@@ -44,18 +44,19 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
   };
 
   function searchPage(searchTerm, start = 1) {
-    const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
-    return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
+    // Updated baseUrl
+    const baseUrl = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(searchTerm)}&f%5Bpage+type%5D=staff+profile`;
+    return $http.get(baseUrl)
       .then(response => {
-        if (!response.data || response.data.length === 0) {
+        // Ensure that you adapt this part to handle the response from the new endpoint
+        if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
           return $scope.results;
         }
         
         $scope.results = $scope.results.concat(response.data);
         $scope.totalResults = $scope.results.length;
         $scope.filteredResults = $scope.results;
-        $scope.$apply();
-        
+
         return response.data.length === 10 ? searchPage(searchTerm, start + 10) : $scope.results;
       });
   }
