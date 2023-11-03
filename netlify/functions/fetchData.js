59e16809eefb3fefb5ab075c9ce0910f082d6d08
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 14:01:42 +0000

    Update fetchData.js

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -8,16 +8,48 @@ async function fetchPageResults(url) {
   const { data } = await axios.get(url);
   const $ = cheerio.load(data);
 
-  // ... rest of the code unchanged
+  const results = await Promise.all($('ul.site-search-results-list li.site-search-results-list-item').map(async (index, element) => {
+    const name = $(element).find('h3 a').text().trim();
+    const profileUrl = $(element).find('h3 a').attr('href');
+    const additionalInfo = $(element).find('.site-search-results-list-item-additional-information').text().trim();
+
+    let expertise = [];
+    let photoUrl = '';
+    let photoAlt = '';
+
+    try {
+      console.log(`Fetching data for profile URL: ${profileUrl}`);
+      const { data: profileData } = await axios.get(profileUrl);
+      const profile$ = cheerio.load(profileData);
+
+      profile$('.staff-profile-areas-of-expertise ul li').each((i, el) => {
+        expertise.push(profile$(el).text());
+      });
+
+      photoUrl = profile$('.staff-profile-overview-profile-picture img').attr('src');
+      photoAlt = profile$('.staff-profile-overview-profile-picture img').attr('alt');
+
+      if (photoUrl && !photoUrl.startsWith('http')) {
+        photoUrl = `https://www.swansea.ac.uk${photoUrl}`;
+      }
+
+    } catch (error) {
+      console.error(`Failed to fetch details for ${profileUrl}`, error);
+    }
+
+    return { name, profileUrl, additionalInfo, expertise, photoUrl, photoAlt };
+  }).get());
+
+  return results;
 }
 
 exports.handler = async function (event) {
   const searchTerm = event.queryStringParameters.q;
-  const start = parseInt(event.queryStringParameters.s, 10) || 1; // Changed default value to 1
+  const start = parseInt(event.queryStringParameters.s, 10) || 0;
   const baseUrl = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(searchTerm)}&f[page type]=staff profile`;
 
   try {
-    const pageUrl = `${baseUrl}&s=${(start - 1) * 10}`; // Adjust s parameter calculation
+    const pageUrl = `${baseUrl}&s=${start * 10}`;
     console.log(`Fetching data from URL: ${pageUrl}`);
     const { data: pageData } = await axios.get(pageUrl);
     const $ = cheerio.load(pageData);
