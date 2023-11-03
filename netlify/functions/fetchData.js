1012ffda4c8a71b93ed82e2574749f4980563de6
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-03 03:13:08 +0000

    Update fetchData.js

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,37 +1,53 @@
-const app = angular.module('SearchApp', []);
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
-  $scope.results = [];
-  $scope.filteredResults = [];
-  $scope.totalResults = 0;
+async function fetchPage(url) {
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
 
-  function searchPage(searchTerm, start = 1) {
-    const FUNCTION_ENDPOINT = '/.netlify/functions/fetchData';
-    $scope.loading = true;
-    return $http.get(FUNCTION_ENDPOINT, { params: { q: searchTerm, s: start } })
-      .then(response => {
-        const { results, totalPages } = response.data;
-        $scope.results = $scope.results.concat(results);
-        $scope.filteredResults = $scope.results;
-        $scope.totalResults = totalPages * 10; // Assuming there are 10 results per page
-        $scope.loading = false;
-        $scope.$apply();
-        return results.length > 0 && start < $scope.totalResults ? searchPage(searchTerm, start + 10) : $scope.results;
-      });
+  // Extract results from the current page
+  const results = $('ul.site-search-results-list li.site-search-results-list-item').map((index, element) => {
+    const name = $(element).find('h3 a').text().trim();
+    const profileUrl = $(element).find('h3 a').attr('href');
+    const additionalInfo = $(element).find('.site-search-results-list-item-additional-information').text().trim();
+
+    return { name, profileUrl, additionalInfo };
+  }).get();
+
+  // Extract the total number of pages from the pagination element
+  const totalPages = $('ul.site-search-results-pagination li.site-search-results-pagination-item').last().prev().find('a').text();
+  const totalPagesNum = parseInt(totalPages) || 1;
+  
+  return { results, totalPages: totalPagesNum };
+}
+
+async function fetchAllPages(searchTerm) {
+  const start = 1;
+  const url = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(searchTerm)}&f[page type]=staff profile&start=${start}`;
+  const { results, totalPages } = await fetchPage(url);
+  const allResults = [...results];
+
+  for(let i = 2; i <= totalPages; i++) {
+    const nextPageUrl = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(searchTerm)}&f[page type]=staff profile&start=${i * 10}`;
+    const { results: nextPageResults } = await fetchPage(nextPageUrl);
+    allResults.push(...nextPageResults);
   }
 
-  $scope.search = function () {
-    $scope.results = [];
-    const searchTerm = $scope.searchTerm;
-    searchPage(searchTerm);
-  };
+  return allResults;
+}
 
-  $scope.filterResults = function () {
-    const additionalSearchTerm = $scope.additionalSearchTerm.toLowerCase();
-    $scope.filteredResults = $scope.results.filter(result =>
-      result.name.toLowerCase().includes(additionalSearchTerm) ||
-      result.additionalInfo.toLowerCase().includes(additionalSearchTerm) ||
-      (result.expertise && result.expertise.some(e => e.toLowerCase().includes(additionalSearchTerm)))
-    );
+exports.handler = async function (event) {
+  const searchTerm = event.queryStringParameters.q;
+  const allResults = await fetchAllPages(searchTerm);
+
+  // Fetch additional data from profile pages (optional)
+  const detailedResults = await Promise.all(allResults.map(async (result) => {
+    // ... (same as your existing code)
+    return { ...result, expertise, photoUrl, photoAlt };
+  }));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ results: detailedResults, totalPages: Math.ceil(detailedResults.length / 10) })
   };
-}]);
+};
