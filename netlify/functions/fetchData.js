ecfbcea728aaf3f50f0d20323536efbdeb9fa89b
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-04 08:55:48 +0000

    Update fetchData.js
    fetch results concurrently to improve speed

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -6,46 +6,49 @@ async function fetchPageResults(url) {
   const { data } = await axios.get(url);
   const $ = cheerio.load(data);
 
-  const results = await Promise.all($('ul.site-search-results-list li.site-search-results-list-item').map(async (index, element) => {
-    const name = $(element).find('h3 a').text().trim();
-    const profileUrl = $(element).find('h3 a').attr('href');
-    const additionalInfo = $(element).find('.site-search-results-list-item-additional-information').text().trim();
+  const results = await Promise.all(
+    $('ul.site-search-results-list li.site-search-results-list-item').map(async (index, element) => {
+      const name = $(element).find('h3 a').text().trim();
+      const profileUrl = $(element).find('h3 a').attr('href');
+      const additionalInfo = $(element).find('.site-search-results-list-item-additional-information').text().trim();
 
-    let expertise = [];
-    let photoUrl = '';
-    let photoAlt = '';
+      let expertise = [];
+      let photoUrl = '';
+      let photoAlt = '';
 
-    try {
-      console.log(`Fetching data for profile URL: ${profileUrl}`);
-      const { data: profileData } = await axios.get(profileUrl);
-      const profile$ = cheerio.load(profileData);
+      try {
+        console.log(`Fetching data for profile URL: ${profileUrl}`);
+        const { data: profileData } = await axios.get(profileUrl);
+        const profile$ = cheerio.load(profileData);
 
-      profile$('.staff-profile-areas-of-expertise ul li').each((i, el) => {
-        expertise.push(profile$(el).text());
-      });
+        profile$('.staff-profile-areas-of-expertise ul li').each((i, el) => {
+          expertise.push(profile$(el).text());
+        });
 
-      photoUrl = profile$('.staff-profile-overview-profile-picture img').attr('src');
-      photoAlt = profile$('.staff-profile-overview-profile-picture img').attr('alt');
+        photoUrl = profile$('.staff-profile-overview-profile-picture img').attr('src');
+        photoAlt = profile$('.staff-profile-overview-profile-picture img').attr('alt');
 
-      if (photoUrl && !photoUrl.startsWith('http')) {
-        photoUrl = `https://www.swansea.ac.uk${photoUrl}`;
+        if (photoUrl && !photoUrl.startsWith('http')) {
+          photoUrl = `https://www.swansea.ac.uk${photoUrl}`;
+        }
+      } catch (error) {
+        console.error(`Failed to fetch details for ${profileUrl}`, error);
       }
 
-    } catch (error) {
-      console.error(`Failed to fetch details for ${profileUrl}`, error);
-    }
-
-    return { name, profileUrl, additionalInfo, expertise, photoUrl, photoAlt };
-  }).get());
+      return { name, profileUrl, additionalInfo, expertise, photoUrl, photoAlt };
+    }).get()
+  );
 
   return results;
 }
 
 exports.handler = async function (event) {
   const searchTerm = event.queryStringParameters.q;
-  const page = parseInt(event.queryStringParameters.p, 10) || 1;  // Default to 1
+  const page = parseInt(event.queryStringParameters.p, 10) || 1; // Default to 1
   const start = (page - 1) * 10 + 1;
-  const baseUrl = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(searchTerm)}&f[page%20type]=staff%20profile`;
+  const baseUrl = `https://www.swansea.ac.uk/search/?c=www-en-meta&q=${encodeURIComponent(
+    searchTerm
+  )}&f[page%20type]=staff%20profile`;
 
   try {
     const pageUrl = `${baseUrl}&s=${start}`;
@@ -61,12 +64,19 @@ exports.handler = async function (event) {
 
     console.log('Total pages:', totalPages);
 
-    const results = await fetchPageResults(pageUrl);
+    // Create an array of promises for fetching results concurrently
+    const fetchResultsPromises = Array.from({ length: totalPages }, (_, i) => {
+      const s = 1 + 10 * i;
+      const pageUrl = `${baseUrl}&s=${s}`;
+      return fetchPageResults(pageUrl);
+    });
 
-    console.log('Total records:', results.length);
+    const allResults = (await Promise.all(fetchResultsPromises)).flat();
+
+    console.log('Total records:', allResults.length);
     return {
       statusCode: 200,
-      body: JSON.stringify({ results: results, totalPages: totalPages }),
+      body: JSON.stringify({ results: allResults, totalPages: totalPages }),
     };
   } catch (error) {
     console.error('Error fetching page results:', error);
