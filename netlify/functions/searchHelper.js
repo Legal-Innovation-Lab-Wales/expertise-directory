023981d14ea1bd854b4e7eeaa32eb10f03602389
awhitjon <148858966+awhitjon@users.noreply.github.com>
2023-11-08 13:35:02 +0000

    Update searchHelper.js

--- netlify/functions/searchHelper.js
+++ netlify/functions/searchHelper.js
@@ -35,63 +35,72 @@ const fetchPageResults = async function (url) {
     }
   };
   
-  fetchAllResults = async function (baseUrl, searchTerm) {
-    // console.time('Total fetchAllResults execution time');
-    // console.log(`Starting fetchAllResults for searchTerm: ${searchTerm}`);
+   fetchAllResults = async function (baseUrl, searchTerm) {
+    console.time('Total fetchAllResults execution time');
+    console.log(`Starting fetchAllResults for searchTerm: ${searchTerm}`);
   
     const searchKey = `${searchTerm}`;
     const cachedResults = await getSearchResultsFromDynamoDB(searchKey);
     if (cachedResults) {
-      // console.log(`Search results for term "${searchTerm}" loaded from DynamoDB.`);
-      // console.timeEnd('Total fetchAllResults execution time');
+      console.log(`Search results for term "${searchTerm}" loaded from DynamoDB.`);
+      console.timeEnd('Total fetchAllResults execution time');
       return { results: cachedResults };
     }
-
-    
+  
     try {
       const firstPageData = await axios.get(`${baseUrl}&s=1`);
       const $ = cheerio.load(firstPageData.data);
       const totalResultsText = $('a.site-search-facets-facet-selected-category-link').text();
       const totalResultsMatch = totalResultsText.match(/\((\d+)\)/);
-      
+  
       const paginationText = $('.sr-only').text();  // "Page 1 of 33"
-        const totalPagesMatch = paginationText.match(/Page \d+ of (\d+)/);
-
-        let totalPages = 0;
-
-        if (totalPagesMatch && totalPagesMatch[1]) {
+      const totalPagesMatch = paginationText.match(/Page \d+ of (\d+)/);
+  
+      let totalPages = 0;
+  
+      if (totalPagesMatch && totalPagesMatch[1]) {
         totalPages = parseInt(totalPagesMatch[1], 10);
-        }
-
-        // console.log(`Total number of pages: ${totalPages}`);
-
-
+      }
+  
+      console.log(`Total number of pages: ${totalPages}`);
+  
       if (!totalResultsMatch) {
-        return { error: 'Could not determine total number of results.', statusCode: 500 };
+        return { error: 'Could not determine the total number of results.', statusCode: 500 };
       }
       const totalResults = parseInt(totalResultsMatch[1], 10);
-      // console.log("totalresults:", totalResults)
-      if (totalPages > 11) {
-        // console.log("advising too many results");
+      console.log("totalresults:", totalResults);
+      if (totalPages > 25) {
+        console.log("advising too many results");
         return { error: `Too many results: ${totalResults}. Please refine your search criteria.`, statusCode: 400 };
       }
   
       let allResults = [];
-      
-      // Fetch from all pages and append results
+  
+      // Fetch from all pages and append results concurrently
+      const fetchPagePromises = [];
       for (let page = 1; page <= totalPages; page++) {
         const pageUrl = `${baseUrl}&s=${1 + (page - 1) * 10}`;
-        const pageResults = await fetchPageResults(pageUrl, searchTerm);
-        allResults = [...allResults, ...pageResults];
+        fetchPagePromises.push(fetchPageResults(pageUrl, searchTerm));
       }
   
-      await saveSearchResultsToDynamoDB(searchKey, allResults);
-      // console.log("Saved new search results to DynamoDB.");
-    
-      // console.timeEnd('Total fetchAllResults execution time');
+      const pageResults = await Promise.all(fetchPagePromises);
+      allResults = pageResults.flat();
+  
+      // Batch save results to DynamoDB
+      const batchSize = 25; // Adjust batch size as needed
+      const batchSavePromises = [];
+      for (let i = 0; i < allResults.length; i += batchSize) {
+        const batch = allResults.slice(i, i + batchSize);
+        batchSavePromises.push(saveSearchResultsToDynamoDB(searchKey, batch));
+      }
+  
+      await Promise.all(batchSavePromises);
+      console.log("Saved new search results to DynamoDB.");
+  
+      console.timeEnd('Total fetchAllResults execution time');
       return { results: allResults }; // Return new results
     } catch (error) {
-      // console.error("Error in fetchAllResults:", error.message);
+      console.error("Error in fetchAllResults:", error.message);
       return { error: error.message, statusCode: 500 }; // Return error message and status code
     }
   };
